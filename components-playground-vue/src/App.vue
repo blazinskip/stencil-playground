623ae03c76325f81cef1306a3d00585cd1062192
blazinskip <blazinski.patryk@gmail.com>
2020-05-25 10:35:44 +0200

    feat: 🎸 add vue project

---
+++ components-playground-vue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <input v-model="message" placeholder="edit me" />
+    <p>Message is: {{ message }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: "App",
+  data: () => ({
+    message: '' as string
+  })
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
